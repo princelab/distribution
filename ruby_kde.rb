fa800e40aead2c07b787d76c4795ed2b39856e88
Ryan Taylor <ryanmt@byu.net>
2012-08-30 17:26:45 -0600

    updates and work on kmeans

--- ruby_kde.rb
+++ ruby_kde.rb
@@ -3,9 +3,6 @@ test_arr = [28.0,596.0,494.0,38.0,717.0,47.0,51.0,891.0,411.0,22.0,31.0,566.0,30
 # test_arr = 10000.times.map { rand }
 $LOAD_PATH << "lib"
 require 'distribution'
-distance = 1
-# Value is distance from datapoint
-Distribution::Normal.pdf(distance)
 
 
 MeshCount = 9200 
@@ -14,9 +11,30 @@ Sigmas = 300
 
 Bandwidth = 100
 
-def nrd0(inputArray, stdDeviation) 
-  bandwidth = ((4 * stdDeviation ** 5)/(3 * inputArray.size))**(1.0/5)
+def nrd0(input_arr, std_deviation = input_arr.standard_deviation) 
+  bandwidth = ((4 * std_deviation ** 5)/(3 * input_arr.size))**(1.0/5)
 end
+
+module Enumerable
+  # Thanks StackOverflow users!
+  # http://stackoverflow.com/questions/7749568/how-can-i-do-standard-deviation-in-ruby
+  def sum
+    self.inject(:+)
+  end
+  def mean
+    self.sum/self.length.to_f
+  end
+  alias average mean
+  def sample_variance
+    m = self.mean
+    sum = self.inject(0){|accum, i| accum + (i-m)**2}
+    sum/(self.length - 1).to_f
+  end
+  def standard_deviation
+    Math.sqrt(self.sample_variance)
+  end
+end
+
 
 def kde(arr, bw = Bandwidth, sigmas= Sigmas, sampling_density = MeshCount)
   # Initialization steps
@@ -43,22 +61,29 @@ resp = kde(test_arr, bandwidth)
 
 x, y = resp.transpose
 
-norm = []
-five = []
-three = []
-ten = []
+require 'k_means'
+kmeans = KMeans.new(test_arr, centroids: 2)
+require 'pry'
+binding.pry
 
-(-10..10).step(0.01) do |i|
-   norm << [i, Distribution::Normal.custom_pdf(i)]
-   five << [i, Distribution::Normal.custom_pdf(i, 5)]
-   three << [i, Distribution::Normal.custom_pdf(i, 3)]
-   ten << [i, Distribution::Normal.custom_pdf(i, 10)]
-end
-
-
+p test_arr.standard_deviation
+p nrd0(test_arr)
+p nrd0(test_arr, Sigmas)
 
 require 'gnuplot'
 =begin
+
+norm = []
+three = []
+five = []
+ten = []
+(-10..10).step(0.01) do |i|
+   norm << [i, Distribution::Normal.custom_pdf(i)]
+   three << [i, Distribution::Normal.custom_pdf(i, 3)]
+   five << [i, Distribution::Normal.custom_pdf(i, 5)]
+   ten << [i, Distribution::Normal.custom_pdf(i, 10)]
+end
+
 Gnuplot.open do |gp|
   Gnuplot::Plot.new( gp ) do |plot|
   
